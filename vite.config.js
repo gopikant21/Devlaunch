1688c5b65c70b7def509202d72404e46deae99c1
shashi <shashi2017razz@gmail.com>
2024-10-09 02:14:51 +0530

    chnaged package.json

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Devlaunch/', // Ensure the base matches your GitHub Pages repo name
   plugins: [react()],
   server: {
     hmr: true
