cd0df40eb71e2af050079ae370200f774b771f83
shashi <shashi2017razz@gmail.com>
2024-10-09 02:51:41 +0530

    chnaged package.json

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Devlaunch/', // Ensure the base matches your GitHub Pages repo name
+  base: process.env.NODE_ENV === 'production' ? '/Devlaunch/' : '/',
   plugins: [react()],
-  server: {
-    hmr: true
-  }
-})
+});
